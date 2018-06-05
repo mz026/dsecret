3f53932f6dd97d6329af838f77539a1210c0794d
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2018-06-05 15:02:01 +0800

    able to return an array of string

--- contracts/DSecret.sol
+++ contracts/DSecret.sol
@@ -18,4 +18,25 @@ contract DSecret {
     secrets.push(Secret(_content, _summary, msg.sender, msg.value));
   }
 
+  function getSecret(uint _index) public view returns(bytes32[2]) {
+    bytes32[2] memory ret;
+    string memory s1 = secrets[0].content;
+    string memory s2 = secrets[1].content;
+    ret[0] = stringToBytes32(s1);
+    ret[1] = stringToBytes32(s2);
+
+    return ret;
+  }
+
+  function stringToBytes32(string memory source) pure internal returns (bytes32 result) {
+    bytes memory tempEmptyStringTest = bytes(source);
+    if (tempEmptyStringTest.length == 0) {
+      return 0x0;
+    }
+
+    assembly {
+      result := mload(add(source, 32))
+    }
+  }
+
 }
--- test/dsecret.js
+++ test/dsecret.js
@@ -1,13 +1,13 @@
 var DSecret = artifacts.require("./DSecret.sol");
 
 contract('DSecret', function(accounts) {
-  it("dummy", function() {
-    return DSecret.deployed().then(function(instance) {
-      return instance.secrets(0);
-    }).then(function(secrets) {
-      console.log(secrets)
-    }).catch(function(e) {
-      console.log('err!', e)
-    });
+  it("dummy", async function() {
+    let instance = await DSecret.deployed()
+    await instance.createSecret('the-content', 'the-summary')
+    await instance.createSecret('the-content-2', 'the-summary-2')
+    let ret = await instance.getSecret.call(0)
+    ret.forEach((r)=> {
+      console.log(web3.toAscii(r))
+    })
   });
 });
